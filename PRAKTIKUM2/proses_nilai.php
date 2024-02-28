95643cd2e5a5bc843b29902653d154ac92d02d5c
MuhamadSabdaPurnama <dutzzbogor@gmail.com>
2024-02-28 11:49:17 +0700

    first commit

---
+++ PRAKTIKUM2/proses_nilai.php
@@ -0,0 +1,13 @@
+<?php 
+$nama = $_GET['nama'];
+$matkul = $_GET['matkul'];
+$uts = $_GET['uts'];
+$uas = $_GET['uas'];
+$tugas = $_GET['tugas'];
+
+// tampikan niai inputan
+echo "Nama lengkap: $nama";
+echo "<br> Mata kuliah: $matkul";
+echo "<br> Nilai uts: $uts";
+echo "<br> Nilai uas: $uas";
+echo "<br> Nilai Tugas: $tugas";
